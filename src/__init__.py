f3b3dd01a57202f46c1da4797771000d9ae3d5f7
hey880 <hey880@naver.com>
2023-02-28 10:54:02 +0900

    add: JWT 추가

--- src/__init__.py
+++ src/__init__.py
@@ -3,20 +3,28 @@ import os
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
+from flask_jwt_extended import JWTManager
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI"),
-            SQLALCHEMY_TRACK_MODIFICATIONS=False # sql alchemy의 이벤트를 처리하는 옵션, 추가적인 메모리를 필요로 하여 꺼두는 것을 추천
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            # sql alchemy의 이벤트를 처리하는 옵션, 추가적인 메모리를 필요로 하여 꺼두는 것을 추천
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY")
         )
     else:
         app.config.from_mapping(test_config)
 
-    db.app=app
+    db.app = app
     db.init_app(app)
+
+    # JWT
+    JWTManager(app)
+
     # blueprint 등록
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
@@ -28,5 +36,5 @@ def create_app(test_config=None):
     @app.get("/hello")
     def say_hello():
         return {"message": "Hello world"}
-    
+
     return app
--- src/auth.py
+++ src/auth.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, jsonify, request
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 from werkzeug.security import check_password_hash, generate_password_hash
 import validators
+from flask_jwt_extended import create_access_token, create_refresh_token
 from src.constants.http_status_codes import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
 
 from src.database import User, db
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
+
 
 @auth.post('/register')
 def register():
@@ -13,7 +16,7 @@ def register():
     email = request.json['email']
     password = request.json['password']
 
-    if len(password)< 6:
+    if len(password) < 6:
         return jsonify({'error': "Password is too short"}), HTTP_400_BAD_REQUEST
 
     if len(username) < 3:
@@ -24,26 +27,55 @@ def register():
 
     if not validators.email(email):
         return jsonify({'error': "Email is not valid"}), HTTP_400_BAD_REQUEST
-    
-    if User.query.filter_by(email=email).first() is not None: #first() : 첫번째에 매칭되는 인스턴스만 달라
+
+    # first() : 첫번째에 매칭되는 인스턴스만 달라
+    if User.query.filter_by(email=email).first() is not None:
         return jsonify({'error': "Email is taken"}), HTTP_409_CONFLICT
-    
+
     if User.query.filter_by(username=username).first() is not None:
         return jsonify({'error': "Username is taken"}), HTTP_409_CONFLICT
 
-    pwd_hash=generate_password_hash(password)
+    pwd_hash = generate_password_hash(password)
 
-    user=User(username=username, password=pwd_hash, email=email) #받은 value들을 db column에 저장
+    user = User(username=username, password=pwd_hash,
+                email=email)  # 받은 value들을 db column에 저장
     db.session.add(user)
     db.session.commit()
 
     return jsonify({
-        'message':"User created",
+        'message': "User created",
         'user': {
             'username': username, 'email': email
-            }
-        }), HTTP_201_CREATED
+        }
+    }), HTTP_201_CREATED
+
+
+@auth.post('/login')
+def login():
+    email = request.json.get('email', '')  # post로 받은 json데이터를 읽음, arg1=key, arg2=defaultvalue
+    password = request.json.get('password', '')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        is_pass_correct = check_password_hash(user.password, password)
+
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=user.id)
+            access = create_access_token(identity=user.id)
+
+            return jsonify({
+                'user': {
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }
+            }), HTTP_200_OK
+
+    return jsonify({'error': 'Wrong credentials'}), HTTP_401_UNAUTHORIZED
+
 
 @auth.get("/me")
 def me():
-    return {"user":"me"}
+    return {"user": "me"}
