2f8e48353303973ba05a350a30c5b03355765c9b
hey880 <hey880@naver.com>
2023-02-28 14:26:54 +0900

    add: put, patch를 같은 동작을 하도록 구현

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 import validators
 from flask_jwt_extended import get_jwt_identity
 from src.database import Bookmark, db
@@ -71,6 +71,41 @@ def get_bookmark(id):
 
     if not bookmark:
         return jsonify({'message': 'Item not found'}), HTTP_400_BAD_REQUEST
+
+    return jsonify({
+        'id': bookmark.id,
+        'url': bookmark.url,
+        'short_url': bookmark.short_url,
+        'visit': bookmark.visits,
+        'body': bookmark.body,
+        'created_at': bookmark.created_at,
+        'updated_at': bookmark.updated_at,
+    }), HTTP_200_OK
+
+
+@bookmarks.put('/<int:id>')
+@bookmarks.patch('/<int:id>')
+@jwt_required()
+def editbookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({'message': 'Item not found'}), HTTP_404_NOT_FOUND
+
+    body = request.get_json().get('body', '')
+    url = request.get_json().get('url', '')
+
+    if not validators.url(url):
+        return jsonify({
+            'error': 'Enter a valid url'
+        }), HTTP_400_BAD_REQUEST
+
+    bookmark.url = url  # url에 request로 받은 새 url 넣음
+    bookmark.body = body  # body에 request로 받은 새 body 넣음
+
+    db.session.commit()
 
     return jsonify({
         'id': bookmark.id,
